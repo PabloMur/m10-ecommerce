7ec14b07091bd2e4a52089136faed57b7e68593f
PabloMur <polillomurillo@gmail.com>
2023-11-24 13:25:57 -0300

    add some components

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,3 +1,14 @@
+import SocialNets from "@/components/SocialNets";
 export default function Footer() {
-  return <footer className="bg-blue-400 h-80">Soy el footer</footer>;
+  return (
+    <footer className="bg-black h-80 flex items-center justify-center">
+      <div>
+        <div>links de navegacion</div>
+        <div>
+          <SocialNets />
+        </div>
+      </div>
+      <p>©2023 compralo</p>
+    </footer>
+  );
 }
