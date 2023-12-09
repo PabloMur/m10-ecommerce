b32d4a08ff1f22013384c41c60aa19d939acd54d
PabloMur <polillomurillo@gmail.com>
2023-12-09 17:59:04 -0300

    searchbar aparentemente arreglada

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -2,16 +2,15 @@
 import LogoutBtn from "./ui/buttons/Logout";
 import MenuLi from "./ui/buttons/MenuLi";
 import { useRecoilValue } from "recoil";
-import { menuActiveAtom, userLoggedAtom } from "@/atoms";
+import { menuActiveAtom } from "@/atoms";
 import LoginBtn from "./ui/buttons/LoginBtn";
 import { ProfileBtn } from "./ui/buttons/ProfileButton";
 
 export default function Menu() {
-  const activeFromAtom = useRecoilValue(userLoggedAtom);
   const menuActive = useRecoilValue(menuActiveAtom);
   return (
     menuActive && (
-      <div className="bg-black absolute top-[12vh] right-0 z-10 w-full p-5 h-[80vh] sm:h-[90vh] sm:w-1/4">
+      <div className="bg-black absolute top-[12vh] right-0 z-20 w-full p-5 h-[80vh] sm:h-[90vh] sm:w-1/4">
         <ul className="flex flex-col gap-10 items-center justify-center h-[100%]">
           <ProfileBtn></ProfileBtn>
           <MenuLi route={"/search"}>Buscar</MenuLi>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,11 +4,15 @@ import Logo from "./ui/Logo";
 import { BurgerButton } from "./ui/buttons/BurgerBtn";
 export default function Navbar() {
   return (
-    <div className=" flex sm:flex-row justify-between p-3 px-10 items-center h-auto min-h-[10vh] relative">
-      <Logo></Logo>
-      <SearchBar></SearchBar>
+    <div className=" flex flex-col sm:flex-row justify-between p-3 items-center h-auto min-h-[10vh] relative">
+      <div className="flex justify-between items-center w-full">
+        <Logo></Logo>
+        <BurgerButton />
+      </div>
+      <div className="bg-black w-full flex justify-center items-center">
+        <SearchBar></SearchBar>
+      </div>
       <Menu></Menu>
-      <BurgerButton />
     </div>
   );
 }
