885ab98c9478177d3f7c7d7b712a76f0a947c777
PabloMur <polillomurillo@gmail.com>
2023-12-08 22:57:59 -0300

    asd

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -2,10 +2,15 @@
 import { ProfileForm } from "@/components/forms/ProfileForm";
 import Loader from "@/components/ui/Loader";
 import { useProtectedPage } from "@/hooks";
+import { useEffect } from "react";
 
 export default function ProfilePage() {
   const protection = useProtectedPage();
-  protection();
+
+  useEffect(() => {
+    protection();
+  }, []);
+
   return (
     <div className="h-[90vh] bg-white flex justify-center items-center relative">
       <Loader></Loader>
